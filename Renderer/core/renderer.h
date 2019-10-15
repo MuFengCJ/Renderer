dd14c97a249f4aa0d47fccec102f5d61158a4641
MuFengCJ <mufengcj@outlook.com>
2019-10-15 18:58:16 +0800

    add renderer

--- Renderer/core/renderer.h
+++ Renderer/core/renderer.h
@@ -1,1 +1,18 @@
-#pragma once
+#ifndef RENDERER_H
+#define RENDERER_H
+
+#include "window.h"
+
+class Renderer
+{
+public:
+	Renderer();
+	~Renderer();
+
+	void render() const;
+
+protected:
+	Window* windowToRender;
+};
+
+#endif
